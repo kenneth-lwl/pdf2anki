a3bbd62726f8fd7ff14efa2f7a8f60bc46485428
kenneth-lwl <kennethlwl03@gmail.com>
2023-07-24 03:08:17 +0800

    added new files

---
+++ pdf2anki.py
@@ -0,0 +1,35 @@
+from pdf2image import convert_from_path
+import os
+import sys
+import shutil
+
+TEMP_FOLDER = "temp"
+
+if len(sys.argv) < 3:
+    print('Invalid number of arguments. Refer to the readme for usage instructions.')
+    exit()
+
+pdf_file = sys.argv[1]
+deck_name = sys.argv[2]
+
+
+out_folder = 'pdf2anki_images_%s' % deck_name
+
+if not os.path.exists(TEMP_FOLDER):
+    os.makedirs(TEMP_FOLDER)
+
+pages = convert_from_path(pdf_file, output_folder=TEMP_FOLDER)
+
+if not os.path.exists(out_folder):
+    os.makedirs(out_folder)
+
+for i in range(0, len(pages)):
+    pages[i].save('%s/Image_%s_%05d.jpg' % (out_folder, deck_name, i), 'JPEG')
+
+
+with open('pdf2anki_%s.csv' % deck_name, 'a') as f:
+    f.write("\n".join(['%s_%05d;<img src="Image_%s_%05d.jpg"/>;<img src="Image_%s_%05d.jpg"/>\n' %
+                 (deck_name, image_number, deck_name, image_number*2, deck_name, image_number * 2 + 1)
+     for image_number in range(0, int(len(pages)/2))]))
+
+shutil.rmtree(TEMP_FOLDER)
